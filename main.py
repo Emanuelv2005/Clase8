3d5b0cb4498bf1d2172e7e22436ce083df096744
Emanuel <emapala143@gmail.com>
2023-05-01 22:27:43 -0500

    Crea una funcionque suma 5 numeros de una lista

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import problema1
+
 
 
 '''
@@ -60,4 +62,8 @@ if __name__ == '__main__':
     print(lista1)
 
     print(lista1[3])
+
+    print()
+
+    problema1.sumar_5_enteros()
 
---
+++ problema1.py
@@ -0,0 +1,27 @@
+def sumar_5_enteros():
+    # definicion de variables
+    lista = []
+    contadorwhile = 0
+    tamaño = 5
+    suma = 0
+
+    # ingresamos los numeros
+    while contadorwhile < tamaño:
+        lista.append(int(input("Ingrese numero "+str(contadorwhile+1) +": ")))
+        contadorwhile +=1
+
+    # sumamos los numeros
+    contadorwhile = 0
+    while contadorwhile < tamaño:
+        suma += lista[contadorwhile]
+        contadorwhile +=1
+
+    print("Los elementos de la lista son:")
+    for numero in lista:
+        print(numero,end=', ')
+
+    print("\n la suma de todos sus elementos es:")
+    print(suma)
+
+    
+
